01d72b27b8f1629a769e3a11db789b603be29f63
vishal kumar ojha <vishal.23bhi10067@vitbhopal.ac.in>
2024-10-16 15:50:55 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hey, I am Vishal Ojha</span> Student at VIT Bhopal University , passionate about leveraging technology to create impactful solutions. With a strong foundation in UI/UX design, Python, JavaScript, Java, and C++, I specialize in developing dynamic and user-friendly applications. My expertise extends to the MERN stack, and I am proficient in frameworks such as React, Django, Flask .In addition to my technical skills, I am dedicated to shaping my personal brand as a versatile web and Android developer. I also have experience as a content writer and video editor, which enhances my ability to communicate complex ideas effectively.I currently serve as the Tech Team Lead at Notion Community VIT Bhopal and also Co-Lead of the technical team at E-Cell VIT Bhopal.I have previously served as the Creative Team Lead, Event Management Team Lead, and Operations Team Lead at Notion Community VIT Bhopal.   I am always eager to learn, grow, and collaborate on projects that push the boundaries of what is possible"
+      ],
+      typeSpeed: 11,
+      contentType: 'html'
+    });
+  }, 100);
